7d0a77e78e8878a1bff9d88c640b1481824e7561
1829122 <100738276+1829122@users.noreply.github.com>
2023-11-29 17:30:10 +0900

    Create Data.py

---
+++ Data/Data.py
@@ -0,0 +1,10 @@
+import csv
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+import seaborn as sns
+from sklearn.model_selection import train_test_split
+!sudo apt-get install -y fonts-nanum
+!sudo fc-cache -fv
+!rm ~/.cache/matplotlib -rf
